581c46deac2106c6d357f2ae6c30832805757d66
Burgeon_Environment <burgeon@burgeon.cat>
2018-12-29 19:06:20 +0100

    old code to .old

--- page.php
+++ page.php
@@ -25,9 +25,9 @@ get_header(); ?>
 				get_template_part( 'template-parts/content', 'page' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+				// if ( comments_open() || get_comments_number() ) :
+				// 	comments_template();
+				// endif;
 
 			endwhile; // End of the loop.
 			?>
@@ -36,5 +36,5 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-parallaxsome_get_sidebar();
+// parallaxsome_get_sidebar();
 get_footer();
