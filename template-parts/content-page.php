111048f306ac53f881d5dda3911e3f79845ee29f
Burgeon_Environment <burgeon@burgeon.cat>
2019-02-14 11:22:07 +0100

    foreach page-content test scandir

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -15,7 +15,7 @@
 	<div class="entry-content">
 		<?php
 			the_content();
-			foreach (glob(get_template_directory_uri() . '/inc/styles/*.css') as $key) {
+			foreach (scandir(get_template_directory_uri() . '/inc/styles/*.css') as $key) {
 				// code...
 				echo '<p>' . $key .'</p><br>';
 			}
