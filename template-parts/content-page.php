4be35fc49f109da2aa4327ea96e69972dcea5470
Burgeon_Environment <burgeon@burgeon.cat>
2019-02-14 11:44:33 +0100

    foreach removed

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -15,16 +15,6 @@
 	<div class="entry-content">
 		<?php
 			the_content();
-			foreach (glob(get_template_directory_uri() . '/inc/styles/') as $key) {
-				// code...
-				echo '<p>' . $key .'</p><br>';
-			}
-			foreach (array_filter(glob(get_template_directory_uri() . '/inc/styles/*.css'),'is_file') as $file) {
-			  // Do something with $file
-			  echo '<p>path: '.$file.'</p>';
-			  echo '<p>st: '.str_replace('.css','',basename($file)).'</p>';
-			  echo '<p>id: '.'locopas-'.str_replace('.css', '', basename($file)).'-style'.'</p>';
-			}
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'locopas' ),
 				'after'  => '</div>',
