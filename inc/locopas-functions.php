107d07ad8571ba6b4879e3c9d882fd53f2217d68
Burgeon_Environment <burgeon@burgeon.cat>
2019-02-14 13:07:15 +0100

    new locopas_styles_extended register first test2

--- inc/locopas-functions.php
+++ inc/locopas-functions.php
@@ -73,20 +73,20 @@ if( ! function_exists( 'locopas_styles_extended' ) ):
 		 * This theme styles several css elements by using separate files inside the theme,
 		 * specifically font, colors, icons, and column width.
 		 */
+
 		foreach (array_filter(glob(get_template_directory_uri().'/inc/styles/*.css'), 'is_file') as $file) {
 			// Do something with $file
 			wp_register_style( 'locopas-'.str_replace('.css', '', basename($file)).'-style',
 											 $file,
-											 array('locopas-style-css'),
-											 $locopas_theme_version,
-										   'all' );
+											 array('locopas-style-css'));
+			wp_enqueue_style('locopas-'.str_replace('.css', '', basename($file)).'-style-css')
 		}
 		// wp_enqueue_style( 'locopas-typography-style', get_template_directory_uri() . '/inc/styles/typography.css', array( 'locopas-style-css' ) );
-		wp_enqueue_style( 'locopas-typography-style-css' );
+		// wp_enqueue_style( 'locopas-typography-style-css' );
 		// wp_enqueue_style( 'locopas-header-style', get_template_directory_uri() . '/inc/styles/header.css', array( 'locopas-style-css' ) );
 		// wp_enqueue_style( 'locopas-contact-style', get_template_directory_uri() . '/inc/styles/contact.css', array( 'locopas-style-css' ) );
-		wp_enqueue_style( 'locopas-widget-style', get_template_directory_uri() . '/inc/styles/widget.css', array( 'locopas-style-css' ) );
-		wp_enqueue_style( 'locopas-footer-style', get_template_directory_uri() . '/inc/styles/footer.css', array( 'locopas-style-css' ) );
+		// wp_enqueue_style( 'locopas-widget-style', get_template_directory_uri() . '/inc/styles/widget.css', array( 'locopas-style-css' ) );
+		// wp_enqueue_style( 'locopas-footer-style', get_template_directory_uri() . '/inc/styles/footer.css', array( 'locopas-style-css' ) );
 	}
 endif;
 add_action( 'wp_enqueue_scripts', 'locopas_styles_extended', 12 );
