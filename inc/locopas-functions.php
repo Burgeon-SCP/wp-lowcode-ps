df4ed397a968b7e0b401630256f728684e3be80e
Burgeon_Environment <burgeon@burgeon.cat>
2019-02-26 18:17:51 +0100

    menu items removed

--- inc/locopas-functions.php
+++ inc/locopas-functions.php
@@ -112,6 +112,32 @@ endif;
 
 add_action( 'admin_enqueue_scripts', 'locopas_admin_scripts' );
 
+/*------------------------------------------------------------------------------------------------------------------*/
+/**
+ * Remove unnecessary menu entries for admin area
+ * extracted from https://codex.wordpress.org/Function_Reference/remove_menu_page
+ *
+ * @since 1.0.1
+ */
+if( ! function_exists( 'locopas_admin_menu' ) ):
+	function locopas_admin_menu() {
+		// remove_menu_page( 'index.php' );                  //Dashboard
+		// remove_menu_page( 'jetpack' );                    //Jetpack*
+		remove_menu_page( 'edit.php' );                   //Posts
+		// remove_menu_page( 'upload.php' );                 //Media
+		// remove_menu_page( 'edit.php?post_type=page' );    //Pages
+		remove_menu_page( 'edit-comments.php' );          //Comments
+		// remove_menu_page( 'themes.php' );                 //Appearance
+		// remove_menu_page( 'plugins.php' );                //Plugins
+		remove_menu_page( 'users.php' );                  //Users
+		// remove_menu_page( 'tools.php' );                  //Tools
+		// remove_menu_page( 'options-general.php' );        //Settings
+	}
+endif;
+
+add_action( 'admin_menu', 'locopas_admin_menu' );
+
+
 
 /*------------------------------------------------------------------------------------------------------------------*/
 if( ! function_exists( 'locopas_parallax_menu_cb' ) ):
