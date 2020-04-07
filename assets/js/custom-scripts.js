a459bb33c4820149dc47ea143391d584ff1be6d3
adria <adria@debian>
2020-04-07 13:09:01 +0200

    UP

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -30,55 +30,55 @@ jQuery(document).ready(function($) {
     /**
 	 * Main Slider
 	 */
-	$('.frontSlider').bxSlider({
-		auto:true,
-		speed:1000,
-		pause:6500,
-		controls:false,
-		mode:'fade',
-        pager:true
-	});
+	// $('.frontSlider').bxSlider({
+	// 	auto:true,
+	// 	speed:1000,
+	// 	pause:6500,
+	// 	controls:false,
+	// 	mode:'fade',
+    //     pager:true
+	// });
 
 	/**
 	 * Main slider height
 	 */
-	if( $('body').hasClass('home') ){
-        /*reduceHeight = 75 ;
-        if( $('#wpadminbar').length ) {
-          reduceHeight = 92;
-        }*/
-        if( $('.lc-front-slider-wrapper').length ) {
-            $(window).resize(function() {
-                var wHeight = ( $(window).height() );
-                $('.lc-front-slider-wrapper').find( '.bx-viewport' ).height(wHeight);
-                $('.single-slide-wrap').height(wHeight);
-            }).resize();
-        }
-    }
+	// if( $('body').hasClass('home') ){
+    //     /*reduceHeight = 75 ;
+    //     if( $('#wpadminbar').length ) {
+    //       reduceHeight = 92;
+    //     }*/
+    //     if( $('.lc-front-slider-wrapper').length ) {
+    //         $(window).resize(function() {
+    //             var wHeight = ( $(window).height() );
+    //             $('.lc-front-slider-wrapper').find( '.bx-viewport' ).height(wHeight);
+    //             $('.single-slide-wrap').height(wHeight);
+    //         }).resize();
+    //     }
+    // }
 
 	/**
 	 * Service section
 	 */
-	$('.service-tab-content .tab-pane').hide();
-	$('.service-nav-tab li').first().addClass('active');
-	$('.service-tab-content .tab-pane').first().addClass('active');
-	$('.service-tab-content .tab-pane').first().show();
-	$('.service-nav-tab li a').on('click', function(){
-		var tabId = $(this).attr('data-tab');
-		$('.service-tab-content .tab-pane').hide();
-		$('.service-tab-content .tab-pane').removeClass('active');
-		$('.service-nav-tab li').removeClass('active');
-		$(this).parent('li').addClass('active');
-		$('#'+tabId).show();
-        $('#'+tabId).addClass('animated slideInRight');
-		$('#'+tabId).addClass('active');
-        $('#section-services').resize();
-	});
+	// $('.service-tab-content .tab-pane').hide();
+	// $('.service-nav-tab li').first().addClass('active');
+	// $('.service-tab-content .tab-pane').first().addClass('active');
+	// $('.service-tab-content .tab-pane').first().show();
+	// $('.service-nav-tab li a').on('click', function(){
+	// 	var tabId = $(this).attr('data-tab');
+	// 	$('.service-tab-content .tab-pane').hide();
+	// 	$('.service-tab-content .tab-pane').removeClass('active');
+	// 	$('.service-nav-tab li').removeClass('active');
+	// 	$(this).parent('li').addClass('active');
+	// 	$('#'+tabId).show();
+    //     $('#'+tabId).addClass('animated slideInRight');
+	// 	$('#'+tabId).addClass('active');
+    //     $('#section-services').resize();
+	// });
 
 	/**
 	 * Testimonials Section
 	 */
-	$('.testiSlider').bxSlider();
+	// $('.testiSlider').bxSlider();
 
 	/**
 	 * Fact Counter
@@ -91,35 +91,35 @@ jQuery(document).ready(function($) {
     /**
      * Portfolio Section
      */
-    $('#psProjects').lightSlider({
-    	item:5,
-    	loop:true,
-    	slideMove:1,
-    	speed:600,
-        enableDrag: false,
-        slideMargin: 0,
-        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        responsive : [
-            {
-                breakpoint:800,
-                settings: {
-                    item:3,
-                    slideMove:1,
-                    slideMargin:6,
-                  }
-            },
-            {
-                breakpoint:480,
-                settings: {
-                    item:1,
-                    slideMove:1
-                  }
-            }
-        ],
-        onSliderLoad: function() {
-           $('.featuredSlider').removeClass( 'cS-hidden' );
-       	}
-    });
+    // $('#psProjects').lightSlider({
+    // 	item:5,
+    // 	loop:true,
+    // 	slideMove:1,
+    // 	speed:600,
+    //     enableDrag: false,
+    //     slideMargin: 0,
+    //     easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+    //     responsive : [
+    //         {
+    //             breakpoint:800,
+    //             settings: {
+    //                 item:3,
+    //                 slideMove:1,
+    //                 slideMargin:6,
+    //               }
+    //         },
+    //         {
+    //             breakpoint:480,
+    //             settings: {
+    //                 item:1,
+    //                 slideMove:1
+    //               }
+    //         }
+    //     ],
+    //     onSliderLoad: function() {
+    //        $('.featuredSlider').removeClass( 'cS-hidden' );
+    //    	}
+    // });
 
     /**
      * Map section
